2b455de6ad180f5064478dbe75c87e1211e65d1f
yasuaki.yamamoto <coolmongo2003@gmail.com>
2021-03-08 00:24:17 +0900

    Add showServlet

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -10,45 +10,25 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-/**
- * Servlet Filter implementation class EncordingFilter
- */
 @WebFilter("/*")
-public class EncordingFilter implements Filter {
+public class EncodingFilter implements Filter {
 
-    /**
-     * Default constructor.
-     */
-    public EncordingFilter() {
-        // TODO Auto-generated constructor stub
+    public EncodingFilter() {
+
     }
 
-    /**
-     * @see Filter#destroy()
-     */
     public void destroy() {
-        // TODO Auto-generated method stub
-    }
 
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
+    }
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 
-        // TODO Auto-generated method stub
-        // place your code here
-
-        // pass the request along the filter chain
         chain.doFilter(request, response);
     }
 
-    /**
-     * @see Filter#init(FilterConfig)
-     */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
+
     }
 
 }
